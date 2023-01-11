53b11b1765f439ec76ce55f88dc84545d6b3fad4
Oxyenniu <oxyenniu@gmail.com>
2023-01-11 23:58:21 +0300

    1

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -8,19 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var buttonTitle: String = "START"
+    private var currentLight = CurrentLight.red
+    var redLight = LightView(color: .red)
+    var orangeLight = LightView(color: .orange)
+    var greenLight = LightView(color: .green)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black
+            
+                .ignoresSafeArea()
+            AllLightsView()
+                .padding(.top, -350)
+            
+            
+            Button(action: startButtonTapped) {
+                Text(buttonTitle)
+                    .frame(width: 150, height: 30)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .bold()
+                    .font(.title)
+                    .foregroundColor(.white)
+            }
+            .padding(.top, 750)
         }
-        .padding()
+        
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    enum CurrentLight {
+        case red, yellow, green
+    }
+    private mutating func startButtonTapped() {
+        if redLight.opacity == 1 || orangeLight.opacity == 1 || greenLight.opacity == 1 { Text("Next")
+        } else {
+            Text("START")
+        }
+        
+        switch currentLight {
+        case .red:
+            greenLight = opacity(0.3) as! LightView
+            redLight = opacity(1) as! LightView
+            currentLight = .yellow
+        case .yellow:
+            redLight = opacity(0.3) as! LightView
+            orangeLight = opacity(1) as! LightView
+            currentLight = .green
+        case .green:
+            greenLight = opacity(1) as! LightView
+            orangeLight = opacity(0.3) as! LightView
+            currentLight = .red
+            
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
---
+++ TrafficLightSwiftUI/RedLightView.swift
@@ -0,0 +1,27 @@
+//
+//  RedLight.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Oxyenniu on 11.01.2023.
+//
+
+import SwiftUI
+
+struct LightView: View {
+    @State var opacity = 0.3
+    let color: Color
+    var body: some View {
+        Circle()
+            .foregroundColor(color)
+            .frame(width: 100)
+            .overlay(Circle().stroke(Color.white, lineWidth: 5))
+            .shadow(radius: 8)
+            .opacity(opacity)
+    }
+}
+
+struct LightView_Previews: PreviewProvider {
+    static var previews: some View {
+        LightView(color: .red)
+    }
+}
